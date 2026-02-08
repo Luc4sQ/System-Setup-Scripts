a83b8c2ee344b75ec5d2fdc22923e706367d3fa8
qualmluc <lucasqualmann@gmx.de>
2026-02-08 23:26:26 +0100

    chore - added extension install

--- linux-arch/initialInstalls.sh
+++ linux-arch/initialInstalls.sh
@@ -4,8 +4,15 @@ echo "Update all packages first."
 sudo pacman -Syu
 
 echo "Install essential packages:"
-sudo pacman -S flatpak flatseal bazaar discord steam godot keepassxc owncloud-client brave-bin thunderbird git audacity
+sudo pacman -S flatpak flatseal bazaar discord steam godot keepassxc owncloud-client brave-bin thunderbird git audacity texlive
 
 echo "Install AUR essentials:"
 yay -S vscodium-bin android-studio
 
+echo "Install codium extensions:"
+codium --install-extension Dart-Code.flutter
+codium --install-extension ms-python.python
+codium --install-extension yzhang.markdown-all-in-one
+codium --install-extension Catppuccin.catppuccin-vsc
+codium --install-extension James-Yu.latex-workshop
+codium --install-extension Gruntfuggly.todo-tree
